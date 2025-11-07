b26f506cd464bc1bc99c899fddc6a5c8786f4dc1
Kavya BM <KavyaM28@users.noreply.github.com>
2025-11-07 15:08:36 +0530

    re-deploy frontend

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,6 @@
 import React, { useState, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../api";
+import api from "../services/api";
 import { AuthContext } from "../context/AuthContext";
 
 const Login = () => {
